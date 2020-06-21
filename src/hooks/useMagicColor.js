7e61d42a86b1b4fec0a004da9d744c186ec8dbc6
tanphat <tanphat1711999@gmail.com>
2020-06-21 16:48:34 +0700

    finish react-hook-basic course

---
+++ src/hooks/useMagicColor.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, useRef } from "react";
+
+function randomColor(currentColor) {
+  const COLOR_LIST = ["red", "green", "yellow"];
+
+  const currentIndex = COLOR_LIST.indexOf(currentColor);
+  let newIndex = currentIndex;
+
+  while (currentIndex === newIndex) {
+    newIndex = Math.trunc(Math.random() * 3);
+  }
+
+  return COLOR_LIST[newIndex];
+}
+
+function useMagicColor() {
+  const [color, setColor] = useState("transparent");
+  const colorRef = useRef("transparent");
+
+  // change color every 1s
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      const newColor = randomColor(colorRef.current);
+      setColor(newColor);
+    }, 1000);
+
+    return () => {
+      clearInterval(colorInterval);
+    };
+  }, []);
+
+  return color;
+}
+
+export default useMagicColor;
